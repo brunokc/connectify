34ab8b871f95fc3d7906b30b28c4054948ba7b82
Bruno Kraychete da Costa <1355373+brunokc@users.noreply.github.com>
2023-10-27 12:41:37 -0700

    Improve range notch support
    Still playing with the idea, not sure if it's going to be used.
    Also, fixed a bug that would cause errors if there were no elements
    using the notches or labels classes.

--- js/advanced-bootstrap-range.js
+++ js/advanced-bootstrap-range.js
@@ -1,23 +1,45 @@
 
 function setupRangeForNotches() {
-    const range = document.querySelectorAll(".range-with-labels")[0];
-    const rangeNotches = document.querySelectorAll(".range-labels")[0];
+    const range = document.querySelectorAll(".range-with-notches")[0];
+    const rangeNotches = document.querySelectorAll(".range-notches")[0];
 
     if (range && range.tagName && range.tagName !== "INPUT") {
         range = range.getElementsByTagName("input")[0];
     }
 
     // Add as many notches as necessary
-    if (range) {
+    if (range && rangeNotches) {
+        const min = parseInt(range.min);
+        const max = parseInt(range.max);
+        const step = parseInt(range.step);
+        for (let i = min; i <= max; i += step) {
+            let notch = document.createElement("span");
+            notch.innerText = i;
+            rangeNotches.appendChild(notch);
+        }
+    }
+};
+
+function setupRangeForLabels() {
+    const range = document.querySelectorAll(".range-with-labels")[0];
+    const rangeLabels = document.querySelectorAll(".range-labels")[0];
+
+    if (range && range.tagName && range.tagName !== "INPUT") {
+        range = range.getElementsByTagName("input")[0];
+    }
+
+    // Add as many notches as necessary
+    if (range && rangeLabels) {
         const min = parseInt(range.min);
         const max = parseInt(range.max);
         const step = parseInt(range.step);
         for (let i = min; i <= max; i += step) {
             let label = document.createElement("span");
             label.innerText = i;
-            rangeNotches.appendChild(label);
+            rangeLabels.appendChild(label);
         }
     }
 };
 
 setupRangeForNotches();
+setupRangeForLabels();
